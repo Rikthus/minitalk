d8dbd5ba8cbfe4e136d8c7a983d49a2f8ddd1e24
Maximilien Pereira <maxperei@z2r5p5.42lyon.fr>
2022-04-18 12:50:07 +0200

    WIP client and server files

--- sources/client.c
+++ sources/client.c
@@ -3,37 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tulipe <tulipe@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: maxperei <maxperei@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 15:29:48 by tulipe            #+#    #+#             */
-/*   Updated: 2022/04/17 23:12:19 by tulipe           ###   ########lyon.fr   */
+/*   Updated: 2022/04/18 11:37:35 by maxperei         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void    send_signal(pid_t server_pid, char *str)
+void	send_signal(pid_t server_pid, char *str)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = 0;
-    while (str[i])
-    {
-        j = 0;
-        while (j < 8)
-        {
-            kill(server_pid, SIGUSR1 + ((str[i] >> j) & 1));
-            j++;
-            usleep(10);
-        }
-        i++;
-    }
+	i = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (j < 8)
+		{
+			kill(server_pid, SIGUSR1 + ((str[i] >> j) & 1));
+			j++;
+			usleep(100);
+		}
+		i++;
+	}
+	while (j > 0)
+	{
+		kill(server_pid, SIGUSR1);
+		j--;
+	}
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 3)
-        return (1);
-    send_signal(argv[1], argv[2]);
+	if (argc < 3)
+		return (1);
+	send_signal(argv[1], argv[2]);
 }
--- sources/server.c
+++ sources/server.c
@@ -3,49 +3,87 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tulipe <tulipe@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: maxperei <maxperei@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 22:47:15 by tulipe            #+#    #+#             */
-/*   Updated: 2022/04/18 03:17:31 by tulipe           ###   ########lyon.fr   */
+/*   Updated: 2022/04/18 12:30:32 by maxperei         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void    handler(int sigtype)
+static	char	*ft_str_join(char *s1, char *s2)
 {
-    static  char    *str;
-    static  int     i = 0;
-    
-    if (i < 8)
-    {
-        if (sigtype == SIGUSR1)
-        {
+	char	*join;
+	int		i;
+	int		j;
 
-        }
-        else if (sigtype == SIGUSR2)
-        {
-
-        }
-        i++;
-    }
-    else
-        i = 0;
-    if (str[i] == '\0')
-        write(1, str, ft_strlen(str));
+	join = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(*join));
+	if (!join)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		join[i] = s1[i++];
+	fork();
+	while (s2[j])
+		join[i + j] = s2[j++];
+	join[i + j] = '\0';
+	if (*s1 != '\0')
+		free(s1);
+	return (join);
 }
 
-int main(int argc, char **argv)
+static	int	create_char(char *c, int i, int sigtype)
 {
-    pid_t               pid;
-    struct sigaction    action;
+	if (sigtype == SIGUSR1)
+	{
+		c = c >> i = 0;
+	}
+	else if (sigtype == SIGUSR2)
+	{
+		c >> i = 1;
+	}
+	return (0);
+}
 
-    pid = getpid();
-    ft_printf("%d\n", pid);
-    action.sa_handler = handler;
-    action.sa_flags = SA_RESTART;
-    sigaction(SIGUSR1, &action, NULL);
-    sigaction(SIGUSR2, &action, NULL);
-    while (1)
-        sleep(42);
+void	handler(int sigtype)
+{
+	static	char	*str = "\0";
+	static	char	*c;
+	static	int		i = 0;
+	static	int		j = 0;
+
+	if (i < 8)
+	{
+		if (create_char(c, i, sigtype) == 1)
+			return ;
+		i++;
+	}
+	else
+	{
+		str = ft_str_join(str, i);
+		if (!str)
+			return ;
+		*c = '\0';
+		i = 0;
+		j++;
+	}
+	if (j > 0 && str[j - 1] == '\0')
+		write(1, str, ft_strlen(str));
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t				pid;
+	struct sigaction	action;
+
+	pid = getpid();
+	ft_printf("%d\n", pid);
+	action.sa_handler = handler;
+	action.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
+	while (1)
+		sleep(42);
 }
